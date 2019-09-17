b6aad6c3c4353b7e838d36c50689a46f7d445f35
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-17 17:39:57 +0700

    Create Header of Search Result Page but still not finish

---
+++ search.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<source src="main.css" type="css">
+</head>
+<body>
+	<div id="header">
+		<h1><span style="color: #12abce">Engi</span>ma</h1>
+		<form style="padding-left: 10px; padding: 2px; border-radius: 5px; border: 2px solid #f0f0f0; height: 25px;">
+			<input style="border: none; height: 100%;" type="text" placeholder="Search movie">
+			<button style="background: none; border: none;">
+				<img width="12px" height="12px" src="img/search-icon.svg">
+			</button>
+		</form>
+		Transaction
+		<div style="float: right">Logout</div>
+	</div>
+</body>
+<style type="text/css">
+	html{
+		height: 100%;
+	}
+	body {
+		background-color: #262626;
+		margin: 0px;
+		height:100%;
+	}
+	h1 {
+		font-size: 16px;
+		margin: 12px;
+	}
+	#header {
+		background-color: #ffffff;
+		display: flex;
+		padding: 5px 150px;
+	}
+	#content {
+		background-color: #ffffff;
+		padding: 50px;
+		width: 80%;
+		height: 100%;
+		margin: auto;
+	}
+</style>
+</html>
