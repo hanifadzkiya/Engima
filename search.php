c22c63d98fe40735e09204ebde9820e2a7718863
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-17 17:54:42 +0700

    Create front end search result item

--- search.php
+++ search.php
@@ -16,6 +16,55 @@
 		Transaction
 		<div style="float: right">Logout</div>
 	</div>
+	<div id="content">
+		<h2>Showing search result for keyword "Captain"</h2>
+		<h4 style="color : #626262">54 result available</h4>
+		<div class="search-result">
+			<table>
+				<tbody>
+					<tr>
+						<td style="width:15%; border-bottom: 1px solid #d2d2d2;">
+							<img width="100px" height="150px" src="img/captain-marvel.jpg">
+						</td>
+						<td style="width:55%; border-bottom: 1px solid #d2d2d2;">
+							<h3>Captain Marvel</h3>
+							<h4>4.75</h4>
+							<p>Captain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls. Living on Earth in 1995, she keeps having recurring memories of another life as U.S. Air Force pilot Carol Danvers.</p>
+						</td>
+						<td style="width:30%; border-bottom: 1px solid #d2d2d2;">
+							View Details
+						</td>
+					</tr>
+					<tr>
+						<td style="width:15%; border-bottom: 1px solid #d2d2d2;">
+							<img width="100px" height="150px" src="img/captain-marvel.jpg">
+						</td>
+						<td style="width:55%; border-bottom: 1px solid #d2d2d2;">
+							<h3>Captain Marvel</h3>
+							<h4>4.75</h4>
+							<p>Captain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls. Living on Earth in 1995, she keeps having recurring memories of another life as U.S. Air Force pilot Carol Danvers.</p>
+						</td>
+						<td style="width:30%; border-bottom: 1px solid #d2d2d2;">
+							View Details
+						</td>
+					</tr>
+					<tr>
+						<td style="width:15%; border-bottom: 1px solid #d2d2d2;">
+							<img width="100px" height="150px" src="img/captain-marvel.jpg">
+						</td>
+						<td style="width:55%; border-bottom: 1px solid #d2d2d2;">
+							<h3>Captain Marvel</h3>
+							<h4>4.75</h4>
+							<p>Captain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls. Living on Earth in 1995, she keeps having recurring memories of another life as U.S. Air Force pilot Carol Danvers.</p>
+						</td>
+						<td style="width:30%; border-bottom: 1px solid #d2d2d2;">
+							View Details
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 <style type="text/css">
 	html{
