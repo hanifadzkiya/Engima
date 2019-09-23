6803bb5a76db008983bcf4f09a3f4f1102fc8e40
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-23 22:03:13 +0700

    Finally Seed

--- Seed/filmGenre.php
+++ Seed/filmGenre.php
@@ -22,15 +22,3 @@ while($row = $result_film->fetch_assoc()) {
 		$film_genre->add($id_film,$id_genre); 
 	}
 }
-
-// for($i = 0;$i<100;$i++){
-// 	$user->add("Hanif Adzkiya ".$i,"hanifadzkiya".$i."@gmail.com","0896499938".$i,"hanifadzkiya".$i,"hanifadzkiya".$i.".png");
-// }
-
-// for($i = 0;$i<100;$i++){
-// 	$user->add("Samantha ".$i,"samantha".$i."@gmail.com","0812214405".$i,"samantha".$i,"samantha".$i.".png");
-// }
-
-// for($i = 0;$i<100;$i++){
-// 	$user->add("Ginting ".$i,"ginting".$i."@gmail.com","081215505".$i,"ginting".$i,"ginting".$i.".png");
-// }
