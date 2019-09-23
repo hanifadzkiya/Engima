c44e50c59208530c6178ce6a61bc15c3a769036b
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-23 18:38:34 +0700

    Seed table Review

--- Model/Review.php
+++ Model/Review.php
@@ -1,6 +1,6 @@
 <?php
-namespace Engima\Model;
-use Engima\Database;
+require_once __DIR__ .'/../Database.php';
+
 Class Review extends Database {
 	private $dbname = "engima";
 	private $tablename = "review";
