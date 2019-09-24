19b248b1cfa6be7d7dbb1aa12b300d3352e16101
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-24 08:57:46 +0700

    Add folder structure for API

--- api/v1/film/index.php
+++ api/v1/film/index.php
@@ -1,1 +1,15 @@
 <?php
+require_once __DIR__.'/../../../Model/Film.php';
+$judul_film = $_GET["judul"];
+$film = new Film();
+$result_film = $film->getByJudul($judul_film);
+$arr_film = Array();
+while($film = $result_film->fetch_assoc()){
+	$arr_film[] = $film;
+}
+$response = Array();
+$response["data"] = $arr_film;
+$response["status"] = 200;
+http_response_code(200); //Set HTTP Response Code
+header('Content-Type: application/json'); 
+print(json_encode($response));
