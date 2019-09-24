9ece12d0303de12f378094ae6c8f203333e6467f
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-24 09:36:18 +0700

    Edit film API

--- api/v1/film/index.php
+++ api/v1/film/index.php
@@ -4,12 +4,18 @@ $judul_film = $_GET["judul"];
 $film = new Film();
 $result_film = $film->getByJudul($judul_film);
 $arr_film = Array();
-while($film = $result_film->fetch_assoc()){
-	$arr_film[] = $film;
-}
 $response = Array();
-$response["data"] = $arr_film;
-$response["status"] = 200;
+if(mysqli_num_rows($result_film) > 0){
+	while($film = $result_film->fetch_assoc()){
+		$arr_film[] = $film;
+	}
+	$response["message"] = "Success get film by title";
+	$response["data"] = $arr_film;
+	$response["status"] = 200;
+} else {
+	$response["message"] = "Film not found";
+	$response["status"] = 200;
+}
 http_response_code(200); //Set HTTP Response Code
 header('Content-Type: application/json'); 
 print(json_encode($response));
