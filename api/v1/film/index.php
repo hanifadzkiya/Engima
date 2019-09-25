527e0767633a8d9c79863e35321b7313e5eb2272
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-26 05:27:00 +0700

    Edit film api

--- api/v1/film/index.php
+++ api/v1/film/index.php
@@ -2,19 +2,31 @@
 require_once __DIR__.'/../../../Model/Film.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	$judul_film = $_GET["judul"];
+	$page = $_GET["page"];
+	if($page == NULL) $page = 1;
 	$film = new Film();
-	$result_film = $film->getByJudul($judul_film);
 	$arr_film = Array();
 	$response = Array();
+    $data = Array();
+    $sql = "SELECT COUNT(*) FROM film WHERE judul LIKE '%".$judul_film."%'";
+    $count_query = $film->runQuery($sql);
+    while($count = $count_query->fetch_assoc()){
+        $data["jumlah_film"] = $count["COUNT(*)"];
+    }
+    $start_row = ($page-1)*5;
+    $total_index = 5;
+    $result_film = $film->getByJudul($judul_film,$start_row,$total_index);
 	if(mysqli_num_rows($result_film) > 0) {
 	    while($film = $result_film->fetch_assoc()){
 	        $arr_film[] = $film;
 	    }
+	    $data["film"] = $arr_film;
 	    $response["message"] = "Success get film by title";
-	    $response["data"] = $arr_film;
+	    $response["data"] = $data;
 	    $response["status"] = 200;
 	} else {
 	    $response["message"] = "Film not found";
+    	$response["data"] = $data;
 	    $response["status"] = 200;
 	}
 	http_response_code(200); //Set HTTP Response Code
