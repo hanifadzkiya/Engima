e069a7ecb4976c61479646df09d03faf943b697b
hanif adzkiya <hanifadzkiya@gmail.com>
2019-09-26 16:38:10 +0700

    Edit film API

--- api/v1/film/index.php
+++ api/v1/film/index.php
@@ -1,21 +1,21 @@
 <?php
 require_once __DIR__.'/../../../Model/Film.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-	$judul_film = $_GET["judul"];
+	$keyword_film = $_GET["keyword"];
 	$page = $_GET["page"];
-	if($page == NULL) $page = 1;
+	if($page == null) $page = 1;
 	$film = new Film();
 	$arr_film = Array();
 	$response = Array();
     $data = Array();
-    $sql = "SELECT COUNT(*) FROM film WHERE judul LIKE '%".$judul_film."%'";
+    $sql = "SELECT COUNT(*) FROM film WHERE judul LIKE '%".$keyword_film."%'";
     $count_query = $film->runQuery($sql);
     while($count = $count_query->fetch_assoc()){
         $data["jumlah_film"] = $count["COUNT(*)"];
     }
     $start_row = ($page-1)*5;
     $total_index = 5;
-    $result_film = $film->getByJudul($judul_film,$start_row,$total_index);
+    $result_film = $film->getByJudul($keyword_film,$start_row,$total_index);
 	if(mysqli_num_rows($result_film) > 0) {
 	    while($film = $result_film->fetch_assoc()){
 	        $arr_film[] = $film;
